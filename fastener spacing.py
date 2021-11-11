2baf16f2f71d0d42e181975b944e483f4e562218
twmkok <93989715+twmkok@users.noreply.github.com>
2021-11-11 15:55:26 +0100

    Update fastener spacing.py
    fixed the formulas

--- fastener spacing.py
+++ fastener spacing.py
@@ -1,7 +1,13 @@
-def fastener_spacing(D_2, w):
-    s_m = (D_2*2, D_2*3)
-    s_c = (D_2*4, D_2*5)
-    e = 1.5*D_2
-    n_m = (w - 2*e)// s_m[0]
-    n_c = (w - 2*e)// s_c[0]
-    return [n_m, n_c]
+import numpy as np
+
+def fastener_spacing(D_2, w, c, Br, s):   #D_2 is the hole diameter, w is the width of the flanges, c is the amount of columns on each side. Br is the total width of the lug. S is the relative spacing compared to the hole size
+    spacing = s*D_2
+    n = (w-3*D_2)//spacing
+    e = (w-n*spacing)/2
+
+    x_pos = np.arange(e, e+n*spacing, spacing)
+    y_posleft = np.arange(D_2*1.5, c*spacing + D_2, spacing)
+    y_posright = np.arange(Br-(c*spacing-D_2*1.5), Br-D_2*1.5, spacing)
+    y_pos = np.append(y_posleft, y_posright)
+
+    return [y_pos, x_pos]
