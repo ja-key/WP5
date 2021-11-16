1ae6715c85e4ea668079decf2aa9a53017810569
twmkok <T.M.Kok@student.tudelft.nl>
2021-11-16 15:51:05 +0100

    finished wp 4.6

--- bearing check.py
+++ bearing check.py
@@ -1,5 +1,5 @@
 import numpy as np
-from  math import sqrt
+from math import sqrt
 def bearing_check(Fx, Fz, n_f, location, x_avg, z_avg, x_pos, z_pos): #Fx is the applied force in the x direction, Fz is the applied force in the z direction, location is the location of the applied force with respect to the coordinate system input is an list of the form [x, z]
     F_ipx = Fx/n_f
     F_ipz = Fz/n_f
@@ -7,15 +7,23 @@ def bearing_check(Fx, Fz, n_f, location, x_avg, z_avg, x_pos, z_pos): #Fx is the
     x_pos = x_pos - x_avg
     z_pos = z_pos - z_avg
     r_lst = []
-    m_lst = []
+    F_lst = []
+    F_tot = []
+    rot = np.array([0, -1], [1, 0])
     for i in x_pos:
         for j in z_pos:
             r = sqrt(i^2 + j^2)
-            u = (i/r, j/r)
+            u = np.array(i/r, j/r)
             r_lst.append([r, u])
     r_lst = np.array(r_lst)
     SUM = np.sum(np.square(r_lst))
     for i in r_lst:
-        F_ipm = (M_cgz*i[1])/SUM
+        F_ipm = (M_cgz*i[0])/SUM
+        F = F_ipm * np.matmul(rot, i[1])
+        F_lst.append((F_ipx + F[0], F_ipz + F[1]))
+    for i in F_lst:
+        F_tot.append(sqrt(i[0]^2 + i[1]^2))
+
+    return (r_lst, F_lst, F_tot)
 
 
