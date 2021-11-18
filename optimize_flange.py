c8106e9cd45d2cb53c24146bcc385ee05d33aa4c
ja-key <76249996+ja-key@users.noreply.github.com>
2021-11-18 16:36:14 +0100

    Update optimize_flange.py

--- optimize_flange.py
+++ optimize_flange.py
@@ -54,7 +54,7 @@ WDs = np.arange(1.1, 5.01, 0.25)
 ts = np.arange(0.0001, 0.005, 0.0005)
 f = 0.006  # distance plate-hole
 
-# materials: 4130 steel, 8630 steel, 2014-t6,2024-t4, 2024-t3, 7075-t6
+# materials: 4130 steel, 8630 steel, 2014-t6, 2024-t4, 2024-t3, 7075-t6
 ftus = np.array([540, 620, 483, 469, 483, 572])*(10**6)
 rhos = np.array([7850, 7850, 2800, 2780, 2780, 2810])
 
@@ -108,7 +108,7 @@ for i in range(2):
                 dlist1.append(deltaP)
                 sigma_bending = F_y/8*(f+width/WD*0.5)*6/(width*t*t)
                 if len(dlist1[:-1]) > 0:
-                    if deltaP <= min(dlist1[:-1]) and sigma_bending < 1.5*ftus[i]:
+                    if deltaP <= min(dlist1[:-1]) and sigma_bending < ftus[i]:
                         bestindex = len(dlist1)-1
                         bestwidth = width
                         bestwd = WD
@@ -118,7 +118,13 @@ for i in range(2):
 
 print("Curve 1:", bestwidth, bestwd, bestt,
       bestmaterial, bestindex, len(dlist1))
-
+deltaP = 0
+bestindex = 0
+bestwidth = 0
+bestwd = 0
+bestt = 0
+bestmaterial = 0
+bestp = 0
 ## Curve 2
 for i in [2, 5]:
     for width in widths:
@@ -129,7 +135,7 @@ for i in [2, 5]:
                 dlist2.append(deltaP)
                 sigma_bending = F_y/8*(f+width/WD*0.5)*6/(width*t*t)
                 if len(dlist2[:-1]) > 0:
-                    if deltaP <= min(dlist2[:-1]) and sigma_bending < 1.5*ftus[i]:
+                    if deltaP <= min(dlist2[:-1]) and sigma_bending < ftus[i]:
                         bestindex = len(dlist2)-1
                         bestwidth = width
                         bestwd = WD
@@ -138,7 +144,13 @@ for i in [2, 5]:
 
 print("Curve 2:", bestwidth, bestwd, bestt,
       bestmaterial, bestindex, len(dlist2))
-
+deltaP = 0
+bestindex = 0
+bestwidth = 0
+bestwd = 0
+bestt = 0
+bestmaterial = 0
+bestp = 0
 ## Curve 4
 for i in [3, 4]:
     for width in widths:
@@ -149,7 +161,7 @@ for i in [3, 4]:
                 dlist4.append(deltaP)
                 sigma_bending = F_y/8*(f+width/WD*0.5)*6/(width*t*t)
                 if len(dlist4[:-1]) > 0:
-                    if deltaP <= min(dlist4[:-1]) and sigma_bending < 1.5*ftus[i]:
+                    if deltaP <= min(dlist4[:-1]) and sigma_bending < ftus[i]:
                         bestindex = len(dlist4)-1
                         bestwidth = width
                         bestwd = WD
