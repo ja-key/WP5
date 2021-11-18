d9ec395e3532f7497c12d51244e40c897d1274da
ja-key <76249996+ja-key@users.noreply.github.com>
2021-11-18 16:20:34 +0100

    curvy curves

--- optimize_flange.py
+++ optimize_flange.py
@@ -43,7 +43,6 @@ def curve7(WD):
 
 
 def get_mass(w, d, t, rho):
-    f = 0.006  # distance plate-hole
     flange_mass = ((f+d/2)*w+0.5*pi*w*w*0.25-pi*d*d*0.25) * rho
     # rectangle + half circle - hole
     return flange_mass
@@ -53,6 +52,7 @@ def get_mass(w, d, t, rho):
 widths = np.arange(1, 20, 0.5)*0.001
 WDs = np.arange(1.1, 5.01, 0.25)
 ts = np.arange(0.0001, 0.005, 0.0005)
+f = 0.006  # distance plate-hole
 
 # materials: 4130 steel, 8630 steel, 2014-t6,2024-t4, 2024-t3, 7075-t6
 ftus = np.array([540, 620, 483, 469, 483, 572])*(10**6)
@@ -106,8 +106,9 @@ for i in range(2):
                 P_u = ftus[i]*(width*(1-1/WD)*t)*curve1(WD)
                 deltaP = abs(Pmax - P_u)
                 dlist1.append(deltaP)
+                sigma_bending = F_y/8*(f+width/WD*0.5)*6/(width*t*t)
                 if len(dlist1[:-1]) > 0:
-                    if deltaP <= min(dlist1[:-1]):
+                    if deltaP <= min(dlist1[:-1]) and sigma_bending < 1.5*ftus[i]:
                         bestindex = len(dlist1)-1
                         bestwidth = width
                         bestwd = WD
@@ -126,8 +127,9 @@ for i in [2, 5]:
                 P_u = ftus[i]*(width*(1-1/WD)*t)*curve2(WD)
                 deltaP = abs(Pmax - P_u)
                 dlist2.append(deltaP)
+                sigma_bending = F_y/8*(f+width/WD*0.5)*6/(width*t*t)
                 if len(dlist2[:-1]) > 0:
-                    if deltaP <= min(dlist2[:-1]):
+                    if deltaP <= min(dlist2[:-1]) and sigma_bending < 1.5*ftus[i]:
                         bestindex = len(dlist2)-1
                         bestwidth = width
                         bestwd = WD
@@ -145,8 +147,9 @@ for i in [3, 4]:
                 P_u = ftus[i]*(width*(1-1/WD)*t)*curve4(WD)
                 deltaP = abs(Pmax - P_u)
                 dlist4.append(deltaP)
+                sigma_bending = F_y/8*(f+width/WD*0.5)*6/(width*t*t)
                 if len(dlist4[:-1]) > 0:
-                    if deltaP <= min(dlist4[:-1]):
+                    if deltaP <= min(dlist4[:-1]) and sigma_bending < 1.5*ftus[i]:
                         bestindex = len(dlist4)-1
                         bestwidth = width
                         bestwd = WD
