862e7ccf30d2ec5a3a2894026f94bc004d038fc5
ja-key <76249996+ja-key@users.noreply.github.com>
2021-11-23 16:52:33 +0100

    Update 4-3.py

--- 4-3.py
+++ 4-3.py
@@ -2,6 +2,52 @@ from math import pi
 # import matplotlib.pyplot as plt
 import numpy as np
 
+
+def curve06(eD):
+    kbr = -0.0507*eD**4 + 0.5353*eD**3 - 2.0619*eD**2 + 3.4741*eD - 1.2321
+    return kbr
+
+
+def curve08(eD):
+    kbr = -0.0583*eD**4 + 0.6169*eD**3 - 2.3885*eD**2 + 4.0549*eD - 1.4803
+    return kbr
+
+
+def curve10(eD):
+    kbr = -0.0585*eD**4 + 0.6268*eD**3 - 2.4618*eD**2 + 4.2581*eD - 1.5711
+    return kbr
+
+
+def curve12(eD):
+    kbr = -0.0583*eD**4 + 0.6258*eD**3 - 2.4758*eD**2 + 4.3467*eD - 1.6309
+    return kbr
+
+
+def curve15(eD):
+    kbr = -0.0505*eD**4 + 0.5666*eD**3 - 2.3461*eD**2 + 4.316*eD - 1.6518
+    return kbr
+
+
+def curve20(eD):
+    kbr = -0.0424*eD**4 + 0.4924*eD**3 - 2.1389*eD**2 + 4.1693*eD - 1.6269
+    return kbr
+
+
+def curve30(eD):
+    kbr = -0.0249*eD**4 + 0.3283*eD**3 - 1.6247*eD**2 + 3.6111*eD - 1.4363
+    return kbr
+
+
+def curve40(eD):
+    kbr = -0.0239*eD**4 + 0.3203*eD**3 - 1.6129*eD**2 + 3.6533*eD - 1.4698
+    return kbr
+
+
+def curve60(eD):
+    kbr = -0.0177*eD**4 + 0.259*eD**3 - 1.4049*eD**2 + 3.401*eD - 1.3866
+    return kbr
+
+
 #input forces
 F_z = 3106.75  # [N] in direction of flight (axial)
 F_y = 1035.58  # [N] in lateral direction (assumed out of s/c)
