2ea3d92ff23b2a5bcacd87747141d2ad264d3651
mscaglioni <76220420+mscaglioni@users.noreply.github.com>
2021-11-25 07:51:31 +0100

    Update 4.8 revenge.py
    zessir

--- 4.8 revenge.py
+++ 4.8 revenge.py
@@ -6,19 +6,19 @@ def pull_through(Fx, Fy, Fz, x_pos, z_pos, x_avg, z_avg, n_f, Ai):
 
     for i in range(n_f):
         denom = []
-        np.append(denom, f"{((x_pos[i]-x_avg)**2 + (z_pos[i]-z_avg)**2) * Ai}")
-        denomenator = np.sum(denom)
+        denom.append(((x_pos[i]-x_avg)**2 + (z_pos[i]-z_avg)**2) * Ai)
+    denominator = np.sum(denom)
 
 
     for i in range(n_f):
         r = []
         r_i = sqrt((x_pos[i] - x_avg) ** 2 + (z_pos[i] - z_avg) ** 2)
-        np.append(r, r_i)
+        r.append(r_i)
         M_cgz = []
         M_cgz_i = Fx * (z_pos[i] - z_avg) + Fz * (x_pos[i] - x_avg)
-        np.append(M_cgz, M_cgz_i)
+        M_cgz.append(M_cgz_i)
         F_pi = Fy/n_f
-        F_pmz = (M_cgz[i] * Ai * r_i) / (denomenator)
+        F_pmz = (M_cgz[i] * Ai * r_i) / (denominator)
         F_yi = []
-        np.append(F_yi,f"{F_pi + F_pmz}")
+        F_yi.append(F_pi + F_pmz)
     return(F_yi)
