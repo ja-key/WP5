c9fbb94ae85b71bf06998f2f8a72ed4c55d8313f
willemijn48 <93989732+willemijn48@users.noreply.github.com>
2021-11-25 15:07:54 +0100

    update

--- A1_final_design.py
+++ A1_final_design.py
@@ -1,1 +1,15 @@
 import numpy as np
+from math import sqrt
+
+from 4.8_r import pull_through
+from 4_NEIN import pull_fail_check
+from 4.10 import force_factor
+# from bearing check import bearing_check      
+e1 =
+e2 =
+h = 
+t1 =
+t2 =
+pull_through() #insert all the necessary variables
+pull_fail_check()
+force_factor()
