d36154bee9b1bacae57f27121f89e7bee179c73c
mattyd20 <M.G.Dinescu@student.tudelft.nl>
2021-11-30 15:36:36 +0100

    Create Column buckling.py

---
+++ Column buckling.py
@@ -0,0 +1,15 @@
+''' Euler Column Buckling '''
+import numpy as np
+
+#Calculates critical buckling stress
+
+#R is radius in [m]
+#L is length in [m]
+#E is E-modulus in [GPa]
+
+def ColBuckle(R, L, E):
+    sigma_cr = np.pi * (E * 10**9) * R*R/(2*L*L)
+    return sigma_cr
+
+print(ColBuckle(1, 5, 1))
+      
