0c9875d3c127bb75c549485cd679c25b4c1e5b31
mattyd20 <M.G.Dinescu@student.tudelft.nl>
2021-12-02 22:45:33 +0100

    fixed a few errors

--- ShellBuckling.py
+++ ShellBuckling.py
@@ -4,10 +4,10 @@ from math import sqrt
 def shellBuckle(p, R, L, E, t1, v):
     Q = (p/E) * (R/t1)**2
     lam = np.ceil(sqrt((12/(np.pi**4))*(L**4)*(1/(t1**2 * R**2))* (1-v**2)))
-    k = lam + (12/(np.pi**4))*(L**4)*(1/(t1**2 * R**2))*(1-v**2)*lam
-    sigma_cr = np.around((1.983 - 0.983*np.exp(-23.14*Q))*k*((np.pi**2 * E)/(12*(1-v**2)))*(t1/L)**2, decimals= 3)
+    k = lam + (12/(np.pi**4))*(L**4)*(1/(t1**2 * R**2))*(1-v**2)*(1/lam)
+    sigma_cr = np.around((1.983 - 0.983*np.exp(-23.14*Q))*k*((np.pi**2 * E)/(12*(1-v**2)))*(t1/L)**2, decimals=3)
     print(sigma_cr/(10**6), "MPa")
     return(sigma_cr)
 
-shellBuckle(1000000, 2, 2, 2000, 0.002, 0.31)
+shellBuckle(1000000, 2, 3, 2000, 0.002, 0.31)
     # k = lambda + (12/(np.pi**4))*(L**4)*(1/(t1**2 * R**2))* (1-v**2)* (1/lambda)
--- StressMeOut.py
+++ StressMeOut.py
@@ -10,12 +10,14 @@ t1 =                   # thickness of the straight part of the tank [m]
 p =                    # internal pressure of the tank [N/m] or [Pa]
 t2 =                   # Thickness of the spherical caps [m]
 rho =                  # Density of material used for tank [kg/m^3]
-
+change = "Yes"         # Checks if a variable has changed in previous loop
+R_ult =                # Ultimate boundary for Radius
+V
 sigma_cr_shell = shellBuckle(p, R, L, E, t1, v)
 sigma_cr_col = ColBuckle(R, L, E)
 
 def TankMass(rho, R, L, t1, t2):
-    mass = rho*(2*np.pi*R*t1*L + 4*np.pi*R*R*t2)
+    mass = rho*(2*np.pi*R*t1*(L-2*R) + 4*np.pi*R*R*t2)
     return mass
 
 
@@ -32,24 +34,39 @@ def TankMass(rho, R, L, t1, t2):
 Area = 2* np.pi * R * t1
 sigma = Fz/Area
 
-if sigma > sigma_cr_shell:
-    
-    print("FAILURE")
-    print("Stress exceeds the critical buckling stress")
+if sigma > sigma_cr_shell and sigma > sigma_cr_col and change == "Yes":
+##    R_new = L/np.pi * np.sqrt(2*sigma/E)
+##    shell_new = shellBuckle(p, R_new, L, E, t1, v)
+##    pos_check = 1.983*2*t1*E/(R_new*np.sqrt(12*(1-v*v)))
+##
+##    if shell_new > sigma:
+##        R = R_new
+##    elif shell_new > pos_check:
+##        R=R
+##        change = "No"
+##        
+##    elif sigma > shell_new:
+##        while sigma > shell_new and change == "Yes":
+##            R_new = R_new + 0.01
+##            shell_new = shellBuckle(p, R_new, L, E, t1, v)
+##            if R_new > R_ult:
+##                change = "No"
+                
 elif sigma > sigma_cr_col:
-    print("Fails under column buckling")
-    R_new = L/np.pi * np.sqrt(2*sigma/E)
-    L_new = np.pi*R * np.sqrt(E/(2*sigma))
-    newList = [R_new, L_new]
-    newNames = ["radius", "length"]
+    #R_new = L/np.pi * np.sqrt(2*sigma/E)
+    L = np.pi*R * np.sqrt(E/(2*sigma))
+    R = (2*np.pi*t1*L+ np.sqrt((2*np.pi*t1*L)**2 - 4*V*(4*np.pi*t1 - 4*np.pi*t2)))/(2*(4*np.pi*t1-4*np.pi*t))
     
-    mass_R = TankMass(rho, R_new, L, t1, t2)
-    mass _L = TankMass(rho, R, L_new, t1, t2)
-
-    massArr = np.array([mass_R, mass_L])
-    minIndex = np.where(massArr == np.min(massArr))[0]
-
-    print("\nIn order to meet buckling requirement either:\n1) Radius has to be changed to", R_new,"m or \n2) Length has to be changed to", L_new,"m.")
+##    newList = [R_new, L_new]
+##    newNames = ["radius", "length"]
+##    
+##    mass_R = TankMass(rho, R_new, L, t1, t2)
+##    mass _L = TankMass(rho, R, L_new, t1, t2)
+##
+##    massArr = np.array([mass_R, mass_L])
+##    minIndex = np.where(massArr == np.min(massArr))[0]
+##
+##    print("\nIn order to meet buckling requirement either:\n1) Radius has to be changed to", R_new,"m or \n2) Length has to be changed to", L_new,"m.")
     for i in minIndex:
         print("To minimize mass to", massArr[i],"kg, change", newNames[i],"to", newList[i], "m")
         
