ffb31006fd6d3e47e5c78943f1a64e349d4e77a4
mattyd20 <M.G.Dinescu@student.tudelft.nl>
2021-12-14 21:13:27 +0100

    finished
    version contains also old code. go to version 2 of code to see "cleaned" code.

--- StressMeOut_v2.py
+++ StressMeOut_v2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ShellBuckling import shellBuckle
 from ColumnBuckling import ColBuckle
-#Fz =                   # Force in the vertical direction on the tank [N]
+
 R = 1.50                   # Radius in metres of the tank [m]
 L = 5.71                   # Total length of the tank in metres [m]
 E = 78 * 10**9                   # Young's Modulus of the material [Pa]
@@ -13,6 +13,7 @@ rho = 2700                 # Density of material used for tank [kg/m^3]
 sigma_y = 490e6           #yield stress of material [Pa]
 col_ind = "N"         # Checks if a variable has changed in previous loop
 shell_ind = "N"
+mAttatch = 0          #Mass of the attatchments
 
 ''' Initial parameters for Titanium '''
 ##E = 114 * 10**9                   # Young's Modulus of the material [Pa]
@@ -23,54 +24,14 @@ shell_ind = "N"
 
 sigma_cr_shell = shellBuckle(p, R, L, E, t1, v)
 sigma_cr_col = ColBuckle(R, L, E)
-print(sigma_cr_shell)
-print(sigma_cr_col)
-
 
 def TankMass(rho, R, L, t1, t2):
-    mass = rho*(2*np.pi*R*t1*(L-2*R) + 4*np.pi*R*R*t2) + 39284
+    mass = rho*(2*np.pi*R*t1*(L-2*R) + 4*np.pi*R*R*t2) + 39284 + mAttatch
     return mass
+
 Fz = 6*9.80665 * TankMass(rho, R, L, t1, t2)
-
-#if sigma_cr_shell > sigma_cr_col:
-##    print("The column buckling is more critical")
-##    sigma_cr = sigma_cr_col
-##elif sigma_cr_col > sigma_cr_shell:
-##    print("The shell buckling is more critical")
-##    sigma_cr = sigma_cr_shell
-##else:
-##    print("wow the stresses are the same, good luck")
-##    sigma_cr = sigma_cr_shell
-
 Area = 2* np.pi * R * t1
 sigma = (Fz/Area)/(10**6)
-
-print("Stress caused by weight is:", sigma)
-print(TankMass(rho, R, L, t1, t2)-39284)
-print(sigma_cr_shell/sigma)
-print(sigma_cr_col/sigma)
-
-t1_upper = (1.05*((TankMass(rho, R, L, t1, t2) - 39284)/rho) - 4*np.pi*R*R*t2)/(2*np.pi*R*(L-2*R))
-print(t1_upper)
-
-
-##if sigma > sigma_cr_shell and sigma > sigma_cr_col and change == "Yes":
-##    R_new = L/np.pi * np.sqrt(2*sigma/E)
-##    shell_new = shellBuckle(p, R_new, L, E, t1, v)
-##    pos_check = 1.983*2*t1*E/(R_new*np.sqrt(12*(1-v*v)))
-##
-##    if shell_new > sigma:
-##        R = R_new
-##    elif shell_new > pos_check:
-##        R=R
-##        change = "No"
-##        
-##    elif sigma > shell_new:
-##        while sigma > shell_new and change == "Yes":
-##            R_new = R_new + 0.01
-##            shell_new = shellBuckle(p, R_new, L, E, t1, v)
-##            if R_new > R_ult:
-##                change = "No"
 
 Rs = []
 Ls = []
@@ -100,6 +61,7 @@ if sigma > sigma_cr_col:
     Area = 2* np.pi * R * t1
     sigma = (Fz/Area)/(10**6)
 
+
 if sigma > sigma_cr_shell:
     shell_ind = "Y"
     t1_old = t1
@@ -114,21 +76,7 @@ if sigma > sigma_cr_shell:
         #Find new critical
         sigma_cr_shell = shellBuckle(p, R, L, E, t1, v)
             
-            
-##    
-####    newList = [R_new, L_new]
-####    newNames = ["radius", "length"]
-####    
-####    mass_R = TankMass(rho, R_new, L, t1, t2)
-####    mass _L = TankMass(rho, R, L_new, t1, t2)
-####
-####    massArr = np.array([mass_R, mass_L])
-####    minIndex = np.where(massArr == np.min(massArr))[0]
-####
-##    print("\nIn order to meet buckling requirement either:\n1) Radius has to be changed to", R_new,"m or \n2) Length has to be changed to", L_new,"m.")
-##    for i in minIndex:
-##        print("To minimize mass to", massArr[i],"kg, change", newNames[i],"to", newList[i], "m")
-##        
+       
 
 if shell_ind = "N" and col_ind = "N":
     L_cr = np.pi*R * np.sqrt(E/(2*sigma))
